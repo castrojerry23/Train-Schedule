1f09446e0188ad462df37d32e8599517bc17b4f1
Jerry Castro <castrojerry23@gmail.com>
2020-01-23 15:20:23 -0600

    installed firebase to root repo and added firebase configuation to train.js

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBzcp7jl7gPULO1-q_qNgiVMHaygLePbFM",
+        authDomain: "train-schedule-638cb.firebaseapp.com",
+        databaseURL: "https://train-schedule-638cb.firebaseio.com",
+        projectId: "train-schedule-638cb",
+        storageBucket: "train-schedule-638cb.appspot.com",
+        messagingSenderId: "498628638158",
+        appId: "1:498628638158:web:2e555f1118ab3e1901679f",
+        measurementId: "G-9T1HWR90XP"
+      };
